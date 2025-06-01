2a811dae5a8a463413922761e72f74fa05d4e1a6
YuHongshuo <yuhsh7@mail2.sysu.edu.cn>
2025-06-01 15:34:31 +0800

    JIKE-ED

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.scss'
-import router from './router'
 import { RouterProvider } from 'react-router-dom'
+import router from './router'
+import { Provider } from 'react-redux'
+import store from './store'
+import 'normalize.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+
+  <Provider store={store}>
     <RouterProvider router={router} />
+  </Provider>
+
 )
